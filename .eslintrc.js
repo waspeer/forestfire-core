2a778c21e1039a8be22d3b01232a15d5e493ea16
Wannes Salomé <mail@wannessalome.nl>
2020-02-15 15:49:55 +0100

    Add custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   // Add custom rules here
-  rules: {}
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+  },
 };
