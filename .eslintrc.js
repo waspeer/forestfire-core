9814c20c4a6c76e460ea579077fa4969ae6acac4
Wannes Salomé <mail@wannessalome.nl>
2020-02-15 16:09:22 +0100

    Add custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
   // Add custom rules here
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
     quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
   },
 };
